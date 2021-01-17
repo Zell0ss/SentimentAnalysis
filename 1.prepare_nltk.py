4dfb14b374342e9725284fdc09c1564553f4acd0
Josem Martin <zelloss@gmail.com>
2021-01-18 00:16:57 +0100

    first steps

---
+++ 1.prepare_nltk.py
@@ -0,0 +1,21 @@
+"""
+TO USE NLTK YOU NEED TO DOWNLOAD SOME
+EXTRA LIBRARIES
+either interactive or with a list
+"""
+import nltk
+
+# nltk.download()
+
+nltk.download(
+    [
+        "names",
+        "stopwords",
+        "state_union",
+        "twitter_samples",
+        "movie_reviews",
+        "averaged_perceptron_tagger",
+        "vader_lexicon",
+        "punkt",
+    ]
+)
---
+++ 2.compiling_data.py
@@ -0,0 +1,107 @@
+# %%
+"""
+# NLTK provides a number of functions out of the box that you can call with few or no arguments
+that will help you meaningfully analyze text before you even touch its machine learning capabilities.
+
+Many of NLTK’s utilities are helpful in preparing your data for more advanced analysis.
+
+"""
+import nltk
+import pandas as pd
+
+
+# %%
+# Check the tokenizer from NLTK
+TEXT = """
+For some quick analysis, creating a corpus could be overkill.
+If all you need is a word list,
+there are simpler ways to achieve that goal. total overkill. Overkill, I tell you."""
+
+tkn_words: list[str] = [w.lower() for w in nltk.word_tokenize(TEXT) if w.isalpha()]
+tkn_words
+
+# %%
+# Working with a corpus: loading the State of the Union corpus in a list of words
+words: list[str] = [w.lower() for w in nltk.corpus.state_union.words() if w.isalpha()]
+# for help with corpus functions help(nltk.corpus.state_union)
+words
+
+# %%
+# remove stopwords
+
+stopwords: list[str] = nltk.corpus.stopwords.words("english")
+words: list[str] = [w for w in words if w.lower() not in stopwords]
+words
+# %%
+tkn_words: list[str] = [w for w in tkn_words if w.lower() not in stopwords]
+tkn_words
+
+# %%
+# Frequence distribution
+
+tkn_fd: nltk.probability.FreqDist = nltk.FreqDist(tkn_words)
+pd.DataFrame(tkn_fd, index=[0])
+
+# %%
+# After building the object, you can use methods like
+# .most_common() and .tabulate() to start visualizing information:
+
+fd: nltk.probability.FreqDist = nltk.FreqDist(words)
+# fd: nltk.probability.FreqDist = words.vocab()  Equivalent
+
+fd.most_common(3), fd.tabulate(5)  # pylint: disable=expression-not-assigned
+
+# %%
+ent_words: nltk.probability.FreqDist = nltk.FreqDist([w for w in words if "ent" in w])
+ent_words.tabulate(10)
+# %%
+# Concordance, the words and its sourroundings.
+# for this we need the original corpus, with stopwords and so on
+
+text = nltk.Text(nltk.corpus.state_union.words())
+text.concordance("america", lines=5)
+# %%
+# Collocation words that come together in Bigrams, trigrams & quadrams
+finder3 = nltk.collocations.TrigramCollocationFinder.from_words(words)
+finder3.ngram_fd.most_common(2), finder3.ngram_fd.tabulate(5)
+# %%
+finder2 = nltk.collocations.BigramCollocationFinder.from_words(words)
+finder2.ngram_fd.tabulate(7)
+# %%
+# analisys with included sentiment analist VADER (only english)
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+
+sia = SentimentIntensityAnalyzer()
+sia.polarity_scores("Wow, NLTK is really powerful!")
+# %%
+# analisys with multilanguage sentiment analist vader-multi
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as SIAmulti
+
+sia = SIAmulti()
+sia.polarity_scores("Wow, NLTK is really powerful!"), sia.polarity_scores(
+    "Pero el que de verdad es bueno es el siamulti"
+), sia.polarity_scores("siamulti es realmente poderoso!"), sia.polarity_scores(
+    "vader es una mierda"
+), sia.polarity_scores(
+    "✈️En dos dias estaré de vuelta en la lluviosa Asturias :-(☔️"
+)
+
+# %%
+#  list of raw tweets as strings isntead word by word.
+tweets = [t.replace("://", "//") for t in nltk.corpus.twitter_samples.strings()]
+# for help with corpus functions help(nltk.corpus.twitter_samples)
+
+# %%
+from random import shuffle
+
+
+def is_positive(tweet: str) -> bool:
+    """True if tweet has positive compound sentiment, False otherwise."""
+    return sia.polarity_scores(tweet)["compound"] > 0
+
+
+shuffle(tweets)
+for tweet in tweets[:10]:
+    print(">", is_positive(tweet), tweet)
+# %%
