21eb448b4e9b1fa233117ad28ac6ae75ea481e3c
Josem Martin <zelloss@gmail.com>
2021-09-05 20:55:06 +0200

    cleaning

--- 2.compiling_data.py
+++ 2.compiling_data.py
@@ -4,6 +4,11 @@
 that will help you meaningfully analyze text before you even touch its machine learning capabilities.
 
 Many of NLTK’s utilities are helpful in preparing your data for more advanced analysis.
+First: some tools out-of-the-box:
+
+- word tokenizers & stop words
+- frequence analisys
+- Concordance and collocations: words & its surroundings
 
 """
 import nltk
@@ -11,63 +16,133 @@ import pandas as pd
 
 
 # %%
-# Check the tokenizer from NLTK
+"""
+Check the word_tokenizer from NLTK:
+function for get the list of tokens (words + signs) from a text passed
+"""
+
 TEXT = """
 For some quick analysis, creating a corpus could be overkill.
 If all you need is a word list,
-there are simpler ways to achieve that goal. total overkill. Overkill, I tell you."""
+there are simpler ways to achieve that goal. total overkill. Overkill, I tell you. 123 12"""
 
 tkn_words: list[str] = [w.lower() for w in nltk.word_tokenize(TEXT) if w.isalpha()]
-tkn_words
+set(tkn_words)
 
 # %%
+"""
+To know how to work with a corpus, lets load one and turn it toa list of words
+"""
 # Working with a corpus: loading the State of the Union corpus in a list of words
 words: list[str] = [w.lower() for w in nltk.corpus.state_union.words() if w.isalpha()]
 # for help with corpus functions help(nltk.corpus.state_union)
 words
 
 # %%
+"""
+stop_words:  “of,” “a,” “the,” and similar.
+ These common words are called stop words, and they can have a negative effect on analysis because ther frequency.
+ We can filter them out.
+"""
 # remove stopwords
 
 stopwords: list[str] = nltk.corpus.stopwords.words("english")
 words: list[str] = [w for w in words if w.lower() not in stopwords]
 words
 # %%
+"""
+lets do this too with the tkn_words we obtained earlier so we work with a smaller list
+"""
 tkn_words: list[str] = [w for w in tkn_words if w.lower() not in stopwords]
 tkn_words
 
+
 # %%
+"""
+1. Creating Frequency Distributions
+A frequency distribution is essentially a table that tells you how many times each word appears
+within a given text. Basic Operation
+
+In NLTK, frequency distributions are a specific object type (nltk.probability.FreqDist)
+This class provides useful operations for word frequency analysis.
+
+To build a frequency distribution with NLTK, construct the FreqDist class with any word list
+"""
 # Frequence distribution
 
 tkn_fd: nltk.probability.FreqDist = nltk.FreqDist(tkn_words)
 pd.DataFrame(tkn_fd, index=[0])
 
 # %%
-# After building the object, you can use methods like
-# .most_common() and .tabulate() to start visualizing information:
-
+# With the complete corpus word list
+# first passed to lowercase
+words = [w.lower() for w in words]
 fd: nltk.probability.FreqDist = nltk.FreqDist(words)
 # fd: nltk.probability.FreqDist = words.vocab()  Equivalent
+pd.DataFrame(fd, index=[0])
 
-fd.most_common(3), fd.tabulate(5)  # pylint: disable=expression-not-assigned
 
 # %%
-ent_words: nltk.probability.FreqDist = nltk.FreqDist([w for w in words if "ent" in w])
-ent_words.tabulate(10)
-# %%
-# Concordance, the words and its sourroundings.
-# for this we need the original corpus, with stopwords and so on
+# After building the object, you can use methods like .most_common() and .tabulate()
+# to to quickly determine frequently used words in a sample:
 
+fd.most_common(3), fd.tabulate(5)
+
+# %%
+# or to locate the words most used wich contains certain letters.
+# You could create frequency distributions of words starting with a particular letter,
+# or of a particular length, or containing certain letters.
+
+fd_ent_words: nltk.probability.FreqDist = nltk.FreqDist([w for w in words if "ry" in w])
+fd_ent_words.tabulate(5)
+
+# %%
+"""2. Concordance and collocations, the words and its sourroundings.
+
+## Concordance
+In the context of NLP, a concordance is a collection of word locations along with their context. 
+You can use concordances to find:
+- How many times a word appears
+- Where each occurrence appears
+- What words surround each occurrence
+In NLTK, you can do this by calling .concordance(). Note that it ignores case
+
+To use it, you need an instance of the nltk.Text class, which can also be constructed with a word list.
+"""
+# for this we need the original corpus, with stopwords and so on so the sentences actually make sense
 text = nltk.Text(nltk.corpus.state_union.words())
+# directly to the console
 text.concordance("america", lines=5)
+
 # %%
-# Collocation words that come together in Bigrams, trigrams & quadrams
+# can be obtained in list format
+concordance_list = text.concordance_list("america", lines=2)
+for entry in concordance_list:
+    print(entry.line)
+
+# %%
+"""Concordance and collocations, the words and its sourroundings.
+
+## Collocation 
+Words that come together in Bigrams, trigrams & quadrams:
+Bigrams: Frequent two-word combinations
+Trigrams: Frequent three-word combinations
+Quadgrams: Frequent four-word combinations
+"""
+
 finder3 = nltk.collocations.TrigramCollocationFinder.from_words(words)
 finder3.ngram_fd.most_common(2), finder3.ngram_fd.tabulate(5)
 # %%
 finder2 = nltk.collocations.BigramCollocationFinder.from_words(words)
 finder2.ngram_fd.tabulate(7)
+
 # %%
+"""
+Using NLTK’s Pre-Trained Sentiment Analyzer (ENGLISH)
+
+NLTK already has a built-in, pretrained sentiment analyzer called 
+VADER (Valence Aware Dictionary and sEntiment Reasoner).
+"""
 # analisys with included sentiment analist VADER (only english)
 from nltk.sentiment import SentimentIntensityAnalyzer
 
@@ -82,7 +157,9 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as SIAmulti
 siam = SIAmulti()
 siam.polarity_scores(
     "Pero el que de verdad es bueno es el siamulti"
-), siam.polarity_scores("siamulti es realmente poderoso!"), siam.polarity_scores(
+), siam.polarity_scores(
+    "Esta libreria, siamulti, es realmente poderosa!"
+), siam.polarity_scores(
     "vader es una mierda"
 ), siam.polarity_scores(
     "✈️En dos dias estaré de vuelta en la lluviosa Asturias :-(☔️"
@@ -97,14 +174,14 @@ tweets = [t.replace("://", "//") for t in nltk.corpus.twitter_samples.strings()]
 from random import shuffle
 
 
-def is_positive(tweet: str) -> bool:
+def is_positive_c_sent(tweet: str) -> bool:
     """True if tweet has positive compound sentiment, False otherwise."""
     return sia.polarity_scores(tweet)["compound"] > 0
 
 
 shuffle(tweets)
 for tweet in tweets[:10]:
-    print(">", is_positive(tweet), tweet)
+    print(">", is_positive_c_sent(tweet), tweet)
 # %%
 positive_review_ids = nltk.corpus.movie_reviews.fileids(categories=["pos"])
 negative_review_ids = nltk.corpus.movie_reviews.fileids(categories=["neg"])
@@ -129,7 +206,8 @@ def is_positive(review_id: str) -> bool:
 
 
 def is_positivec(review_id: str) -> bool:
-    """True if the countof all sentence with positive compound scores is higher than the negative."""
+    """True if the count of all sentence with positive compound scores
+    is higher than the negative."""
     text = nltk.corpus.movie_reviews.raw(review_id)
     scores_pos = [
         1
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # SentimentAnalysis
 https://realpython.com/python-nltk-sentiment-analysis/
+
+
+## What is Sentiment Analisis
+
+Sentiment analysis is the practice of using algorithms to classify various samples of related text into overall positive and negative categories. Sentiment analysis can help you determine the ratio of positive to negative engagements about a specific topic. 
+
+We will use NLTK for processing the text data and perform sentiment analysis.
+
+## NLTK library
+
+The NLTK library contains various utilities that allow you to effectively manipulate and analyze linguistic data. We will focus in its text classifiers using them for sentiment analysis.
+
+**corpus and corpora**:  Pre-processed bodies of text used to train the models. A corpus is a large collection of related text samples, but inthe context of NLTK, means compiled with features for natural language processing (NLP), such as categories and numerical scores for particular features.
+
+### Installing NLTK
+
+You need not only to install NLTK but use its download manager to get its resources
+
+```` shell
+$ .venv/bin/activate
+$ pip install nltk
+$ python
+>>> import nltk
+>>> nltk.download()
+````
+
+NLTK will display its downloader and the resources in o https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/index.xml
+
+For spanish we have cess_esp, inaugural, machado, opinion_lexicon & spanish_grammars
+
+For this tutorial we need installed:
+- names: A [list of common English names](http://www-2.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/) compiled by Mark Kantrowitz
+- stopwords: A list of really common words, like articles, pronouns, prepositions, and conjunctions
+- state_union: A sample of transcribed [State of the Union](https://en.wikipedia.org/wiki/State_of_the_Union) addresses by different US presidents, compiled by Kathleen Ahrens
+- twitter_samples: A list of social media phrases posted to Twitter
+- movie_reviews: [Two thousand movie reviews](http://www.cs.cornell.edu/people/pabo/movie-review-data/) categorized by Bo Pang and Lillian Lee
+- averaged_perceptron_tagger: A data model that NLTK uses to categorize words into their part of speech
+- vader_lexicon: A scored [list of words and jargon](https://github.com/cjhutto/vaderSentiment), specifically attuned to sentiments expressed in social media, that NLTK references when performing sentiment analysis, created by C.J. Hutto and Eric Gilbert
+- punkt: A data model created by Jan Strunk that NLTK uses to split full texts into word lists
+
+For this you can run the script `1.prepare_nltk.py`
+
+```` shell
+>>> import nltk
+
+>>> nltk.download([
+    "names",
+    "stopwords",
+    "state_union",
+    "twitter_samples",
+    "movie_reviews",
+    "averaged_perceptron_tagger",
+    "vader_lexicon",
+    "punkt",
+])
+````
+Once ready go to the `2.compilig_data.py`
